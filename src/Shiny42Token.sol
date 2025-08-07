e29b6a09201480fa7b116e75ca7c5c4b44b1ce40
Tiago Martins <tiago86martins@gmail.com>
2025-08-07 13:06:20 +0100

    added getters

--- src/Shiny42Token.sol
+++ src/Shiny42Token.sol
@@ -48,8 +48,8 @@ contract Shiny42Token is ERC20, Ownable {
                             STATE VARIABLES
     //////////////////////////////////////////////////////////////*/
     mapping(address user => uint256 balance) s_userBalance;
-    mapping(address token => uint256 totalSupply) s_totalTokenSupply;
     uint256 s_totalShiny42TokenSupply;
+    uint256 s_totalSepoliaSupply;
 
     /*//////////////////////////////////////////////////////////////
                                  EVENTS
@@ -69,31 +69,71 @@ contract Shiny42Token is ERC20, Ownable {
                                  ERRORS
     //////////////////////////////////////////////////////////////*/
     error Shiny42Token__NeedsToBeMoreThanZero();
+
     /*//////////////////////////////////////////////////////////////
                                FUNCTIONS
     //////////////////////////////////////////////////////////////*/
-    constructor() ERC20("Shiny42Token", "S42T") Ownable(msg.sender) {}
+    constructor() ERC20("Shiny42Token", "S42T") Ownable(msg.sender) {
+        s_totalShiny42TokenSupply = 0;
+        s_totalSepoliaSupply = 0;
+    }
 
     /**
      * @notice Mints Shiny42Tokens to the user
      */
-    function mint(address _account, uint256 _value) private onlyOwner {
+    function mint(address _account, uint256 _value) internal {
         _mint(_account, _value);
     }
 
     /**
      * @notice Burns Shiny42Tokens from the user
      */
-    function burn(address _account, uint256 _value) private onlyOwner {
+    function burn(address _account, uint256 _value) internal {
         _burn(_account, _value);
     }
 
     /**
      * @notice Swaps other tokens for Shiny42Tokens. A user can only swap for officially recognized tokens.
      */
-    function getShiny42Tokens(address _user, uint256 _amount, address _token) public moreThanZero(_amount) onlyOwner() {
-        s_userBalance += _amount;
-        s_totalTokenSupply[_token]
+    function mintShiny42Tokens(address _user, uint256 _amount) public moreThanZero(_amount) onlyOwner() {
+        s_userBalance[_user] += _amount;
+        s_totalSepoliaSupply += _amount;
+        s_totalShiny42TokenSupply += _amount;
         mint(_user, _amount);
+    }
+
+    /**
+     * @notice Swaps other tokens for Shiny42Token. A user can only swap for officially recognized tokens.
+     */
+    function exchangeShiny42TokensforSepolia(address _user, uint256 _amount) public moreThanZero(_amount) onlyOwner() {
+        s_userBalance[_user] -= _amount;
+        s_totalSepoliaSupply -= _amount;
+        s_totalShiny42TokenSupply -= _amount;
+        burn(_user, _amount);
+    }
+
+    /**
+     * @notice Returns the balance of a user
+     * @param _user The address of the user
+     * @return The balance of the user in Shiny42Token
+     */
+    function getUserBalance(address _user) public view returns (uint256) {
+        return s_userBalance[_user];
+    }
+
+    /**
+     * @notice Gets the total supply of Shiny42Token
+     * @return The total supply of Shiny42Token
+     */
+    function getTotalShiny42TokenSupply() public view returns (uint256) {
+        return s_totalShiny42TokenSupply;
+    }
+
+    /**
+     * @notice Gets the total supply of Sepolia tokens
+     * @return The total supply of Sepolia tokens
+     */
+    function getTotalSepoliaSupply() public view returns (uint256) {
+        return s_totalSepoliaSupply;
     }
 }
