1a88a167196978f25ce7b05f61c9bc78db29594d
Tiago Martins <tiago86martins@gmail.com>
2025-08-08 14:28:51 +0100

    added tests

---
+++ script/Shiny42Token.s.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+import {Script, console2} from "forge-std/Script.sol";
+import {Shiny42Token} from "src/Shiny42Token.sol";
+
+contract Shiny42TokenDeploy is Script {
+    function run() external returns (Shiny42Token shiny42Token) {
+        vm.startBroadcast();
+        shiny42Token = new Shiny42Token();
+        console2.log("Created a new Shiny42Token contract!");
+        console2.log("Adress: ", address(shiny42Token));
+        vm.stopBroadcast();
+    }
+}
